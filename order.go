235014ede61191eef37e7e88695f11c2b85ac81b
rlecomte19 <romanlecomte@gmail.com>
2024-01-08 13:48:01 +0100

    Update fields in SQL queries

--- order.go
+++ order.go
@@ -5,28 +5,35 @@ import (
 )
 
 type Order struct {
-	ID              int    `json:"id"`
-	UserID          string `json:"user_id"`
-	ClusterName     string `json:"cluster_name"`
-	HasControlPlane bool   `json:"has_control_plane"`
-	HasMonitoring   bool   `json:"has_monitoring"`
-	HasAlerting     bool   `json:"has_alerting"`
-	StorageSize     int    `json:"storage_size"`
+	ID                int    `json:"id"`
+	UserID            string `json:"user_id" validate:"required,uuid"`
+	ClusterName       string `json:"cluster_name" validate:"required,min=2,max=63,alphanum,startswithalphanum,endswithalphanum"`
+	HasControlPlane   bool   `json:"has_control_plane" validate:"required"`
+	HasMonitoring     bool   `json:"has_monitoring" validate:"required"`
+	HasAlerting       bool   `json:"has_alerting" validate:"required"`
+	ImageStorage      int    `json:"images_storage" validate:"required"`
+	MonitoringStorage int    `json:"monitoring_storage" validate:"required"`
 }
 
 func (o *Order) GetOrder(db *sql.DB) error {
-	columns := "user_id, cluster_name, has_control_plane,has_monitoring,has_alerting,storage_size"
+	columns := "user_id, cluster_name, has_control_plane,has_monitoring,has_alerting,images_storage, monitoring_storage"
 
 	return db.QueryRow("SELECT "+columns+" FROM orders WHERE id=$1", o.ID).
-		Scan(&o.UserID, &o.ClusterName, &o.HasControlPlane, &o.HasMonitoring, &o.HasAlerting, &o.StorageSize)
+		Scan(&o.UserID,
+			&o.ClusterName,
+			&o.HasControlPlane,
+			&o.HasMonitoring,
+			&o.HasAlerting,
+			&o.ImageStorage,
+			&o.MonitoringStorage)
 }
 
 func (o *Order) UpdateOrder(db *sql.DB) error {
-	columns := "user_id=$1, cluster_name=$2, has_control_plane=$3,has_monitoring=$4,has_alerting=$5,storage_size=$6"
+	columns := "user_id=$1, cluster_name=$2, has_control_plane=$3,has_monitoring=$4,has_alerting=$5,images_storage=$6, monitoring_storage=$7"
 
 	_, err :=
 		db.Exec("UPDATE orders SET "+columns+" WHERE id=$7",
-			o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.StorageSize, o.ID)
+			o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.ImageStorage, o.MonitoringStorage, o.ID)
 
 	return err
 }
@@ -39,8 +46,8 @@ func (o *Order) DeleteOrder(db *sql.DB) error {
 
 func (o *Order) CreateOrder(db *sql.DB) error {
 	err := db.QueryRow(
-		"INSERT INTO orders(user_id, cluster_name, has_control_plane, has_monitoring, has_alerting, storage_size) VALUES($1, $2, $3, $4, $5, $6) RETURNING id",
-		o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.StorageSize).Scan(&o.ID)
+		"INSERT INTO orders(user_id, cluster_name, has_control_plane, has_monitoring, has_alerting, images_storage, monitoring_storage) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+		o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.ImageStorage, o.MonitoringStorage).Scan(&o.ID)
 
 	if err != nil {
 		return err
@@ -51,7 +58,7 @@ func (o *Order) CreateOrder(db *sql.DB) error {
 
 func GetOrders(db *sql.DB, start, count int) ([]Order, error) {
 	rows, err := db.Query(
-		"SELECT id, user_id,cluster_name,has_control_plane,has_monitoring,has_alerting,storage_size FROM orders LIMIT $1 OFFSET $2",
+		"SELECT id, user_id,cluster_name,has_control_plane,has_monitoring,has_alerting,images_storage,monitoring_storage FROM orders LIMIT $1 OFFSET $2",
 		count, start)
 
 	if err != nil {
@@ -64,7 +71,7 @@ func GetOrders(db *sql.DB, start, count int) ([]Order, error) {
 
 	for rows.Next() {
 		var o Order
-		if err := rows.Scan(&o.ID, &o.UserID, &o.ClusterName, &o.HasControlPlane, &o.HasMonitoring, &o.HasAlerting, &o.StorageSize); err != nil {
+		if err := rows.Scan(&o.ID, &o.UserID, &o.ClusterName, &o.HasControlPlane, &o.HasMonitoring, &o.HasAlerting, &o.ImageStorage, &o.MonitoringStorage); err != nil {
 			return nil, err
 		}
 		orders = append(orders, o)
