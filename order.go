d4a156846263eb93c73c52ec5b24c5ea6387323b
rlecomte19 <romanlecomte@gmail.com>
2024-01-24 15:30:47 +0100

    [FEAT] Adding paypal information for orders

--- order.go
+++ order.go
@@ -4,6 +4,11 @@ import (
 	"database/sql"
 )
 
+type OrderFullInfos struct {
+	AppOrder    Order              `json:"app_order"`
+	PaypalOrder PaypalOrderDetails `json:"paypal_order"`
+}
+
 type Order struct {
 	ID                int    `json:"id"`
 	PaypalID          string `json:"paypal_id" validate:"required"`
@@ -16,11 +21,36 @@ type Order struct {
 	MonitoringStorage int    `json:"monitoring_storage" validate:"required_with=has_monitoring"`
 }
 
+type PaypalOrderDetails struct {
+	ID            string         `json:"id"`
+	Intent        string         `json:"intent"`
+	Status        string         `json:"status"`
+	PurchaseUnits []PurchaseUnit `json:"purchase_units"`
+	CreateTime    string         `json:"create_time"`
+}
+
+type PurchaseUnit struct {
+	Reference string `json:"reference_id"`
+	Amount    Amount `json:"amount"`
+	Payee     Payee  `json:"payee"`
+}
+
+type Amount struct {
+	Currency string `json:"currency_code"`
+	Value    string `json:"value"`
+}
+
+type Payee struct {
+	Email    string `json:"email_address"`
+	Merchant string `json:"merchant_id"`
+}
+
 func (o *Order) GetOrder(db *sql.DB) error {
-	columns := "user_id, cluster_name, has_control_plane,has_monitoring,has_alerting,images_storage, monitoring_storage"
+	columns := "paypal_id, user_id, cluster_name, has_control_plane,has_monitoring,has_alerting,images_storage, monitoring_storage"
 
 	return db.QueryRow("SELECT "+columns+" FROM orders WHERE id=$1", o.ID).
-		Scan(&o.UserID,
+		Scan(&o.PaypalID,
+			&o.UserID,
 			&o.ClusterName,
 			&o.HasControlPlane,
 			&o.HasMonitoring,
@@ -30,11 +60,11 @@ func (o *Order) GetOrder(db *sql.DB) error {
 }
 
 func (o *Order) UpdateOrder(db *sql.DB) error {
-	columns := "user_id=$1, cluster_name=$2, has_control_plane=$3,has_monitoring=$4,has_alerting=$5,images_storage=$6, monitoring_storage=$7"
+	columns := "paypal_id=$1, user_id=$2, cluster_name=$3, has_control_plane=$4,has_monitoring=$5,has_alerting=$6,images_storage=$7, monitoring_storage=$8"
 
 	_, err :=
-		db.Exec("UPDATE orders SET "+columns+" WHERE id=$8",
-			o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.ImageStorage, o.MonitoringStorage, o.ID)
+		db.Exec("UPDATE orders SET "+columns+" WHERE id=$9",
+			o.PaypalID, o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.ImageStorage, o.MonitoringStorage, o.ID)
 
 	return err
 }
@@ -46,9 +76,10 @@ func (o *Order) DeleteOrder(db *sql.DB) error {
 }
 
 func (o *Order) CreateOrder(db *sql.DB) error {
+
 	err := db.QueryRow(
-		"INSERT INTO orders(user_id, cluster_name, has_control_plane, has_monitoring, has_alerting, images_storage, monitoring_storage) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
-		o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.ImageStorage, o.MonitoringStorage).Scan(&o.ID)
+		"INSERT INTO orders(paypal_id, user_id, cluster_name, has_control_plane, has_monitoring, has_alerting, images_storage, monitoring_storage) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
+		o.PaypalID, o.UserID, o.ClusterName, o.HasControlPlane, o.HasMonitoring, o.HasAlerting, o.ImageStorage, o.MonitoringStorage).Scan(&o.ID)
 
 	if err != nil {
 		return err
@@ -57,10 +88,22 @@ func (o *Order) CreateOrder(db *sql.DB) error {
 	return nil
 }
 
-func GetOrders(db *sql.DB, start, count int) ([]Order, error) {
-	rows, err := db.Query(
-		"SELECT id, user_id,cluster_name,has_control_plane,has_monitoring,has_alerting,images_storage,monitoring_storage FROM orders LIMIT $1 OFFSET $2",
-		count, start)
+func GetOrders(db *sql.DB, start int, count int, userID ...string) ([]Order, error) {
+
+	queryString := "SELECT id, paypal_id, user_id, cluster_name, has_control_plane, has_monitoring, has_alerting, images_storage, monitoring_storage FROM orders "
+	var args []interface{}
+
+	if userID != nil {
+		queryString += "WHERE user_id = $1 "
+		args = append(args, userID[0])
+		queryString += "LIMIT $2 OFFSET $3"
+	} else {
+		queryString += "LIMIT $1 OFFSET $2"
+	}
+
+	args = append(args, count, start)
+
+	rows, err := db.Query(queryString, args...)
 
 	if err != nil {
 		return nil, err
@@ -72,7 +115,7 @@ func GetOrders(db *sql.DB, start, count int) ([]Order, error) {
 
 	for rows.Next() {
 		var o Order
-		if err := rows.Scan(&o.ID, &o.UserID, &o.ClusterName, &o.HasControlPlane, &o.HasMonitoring, &o.HasAlerting, &o.ImageStorage, &o.MonitoringStorage); err != nil {
+		if err := rows.Scan(&o.ID, &o.PaypalID, &o.UserID, &o.ClusterName, &o.HasControlPlane, &o.HasMonitoring, &o.HasAlerting, &o.ImageStorage, &o.MonitoringStorage); err != nil {
 			return nil, err
 		}
 		orders = append(orders, o)
